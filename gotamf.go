640f4d579433e66a3e3e94bab6534048c1546ce8
Jake Hiney <hiney.jake@outlook.com>
2021-11-04 14:06:23 -0500

    hello world

--- gotamf.go
+++ gotamf.go
@@ -1,10 +1,9 @@
-package gotamf
+package main
 
 import (
 	"fmt"
 )
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func main() {
+	fmt.Println("Hello, World!")
 }
